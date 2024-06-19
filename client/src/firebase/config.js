bf23a38cfc318add8fc780d68145303c32eff35d
Vincy-789 <166099044+Vincy-789@users.noreply.github.com>
2024-06-19 10:49:05 +0530

    Update config.js

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKU-m6HyAsL-5eoVQQS2OXhRux_sb2G8U",
-  authDomain: "meet-app-6b4c7.firebaseapp.com",
-  projectId: "meet-app-6b4c7",
-  storageBucket: "meet-app-6b4c7.appspot.com",
-  messagingSenderId: "1044322632731",
-  appId: "1:1044322632731:web:2e2ba4763f64849d37a4be",
-  measurementId: "G-G2BEM8934W"
+  apiKey: "process.env.API_KEY",
+  authDomain: "process.env.AUTH_DOMAIN",
+  projectId: "process.env.PPROJECT_ID",
+  storageBucket: "process.env.STORAGE",
+  messagingSenderId: "process.env.MSG_SEND",
+  appId: "process.env.ID",
+  measurementId: "process.env.MEASURE_ID"
 };
 
 const app = initializeApp(firebaseConfig);
